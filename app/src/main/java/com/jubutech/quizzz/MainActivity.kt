98708cc7363f901e227088c8da38277ec68b0c0b
Jobayer Ahmed <jubu11469@gmail.com>
2024-09-09 11:11:02 +0600

    A basic Quiz functionality

--- app/src/main/java/com/jubutech/quizzz/MainActivity.kt
+++ app/src/main/java/com/jubutech/quizzz/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.jubutech.quizzz.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -14,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        Thread.sleep(2000)
+        installSplashScreen()
+
         setContentView(binding.root)
 
         binding.playtonextbtn.setOnClickListener {
